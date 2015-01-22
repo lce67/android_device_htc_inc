3f9d69a3d6bafa9467d3be9ec077b05e5fbfbdfa
lce67 <lce67@lce67s-Mac-Pro.local>
2015-01-21 19:50:40 -0600

    init commit

---
+++ BoardConfig.mk
@@ -0,0 +1,91 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := false
+
+# inherit from the proprietary version
+-include vendor/htc/inc/BoardConfigVendor.mk
+# inherit common defines for all qsd8k devices
+include device/htc/inc/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := inc
+
+# Legacy support flags
+BOARD_USE_LEGACY_TRACKPAD := true
+
+BOARD_HAS_SCREEN_OFF_FLICKER := true
+
+TARGET_ARCH_LOWMEM := true
+
+# RIL
+BOARD_USE_NEW_LIBRIL_HTC := true
+BOARD_USES_LEGACY_RIL := true
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_BASE := 0x20000000
+BOARD_KERNEL_NEW_PPPOX := true
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := inc
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun%d/file
+
+# cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 000a0000 00020000 "misc"
+# mtd1: 00480000 00020000 "recovery"
+# mtd2: 00300000 00020000 "boot"
+# mtd3: 0f800000 00020000 "system"
+# mtd4: 000a0000 00020000 "local"
+# mtd5: 02800000 00020000 "cache"
+# mtd6: 09500000 00020000 "datadata"
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00300000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0f800000   # limited so we enforce room to grow
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x09500000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Try to build the kernel
+BUILD_KERNEL := true
+# Kernel directory
+TARGET_KERNEL_SOURCE := kernel/htc/qsd8k
+# Custom Kernel Config
+TARGET_KERNEL_CONFIG := $(ANDROID_BUILD_TOP)/device/htc/inc/kernel-config/cm_inc_defconfig
+# Use GCC 4.7 toolchain
+KERNEL_TOOLCHAIN_PREFIX := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+
+TARGET_RECOVERY_FSTAB := device/htc/inc/prebuilt/root/fstab.inc
+
+BOARD_HAS_DATADATA := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p1
+BOARD_DATA_FILESYSTEM := auto
+BOARD_CACHE_DEVICE := /dev/block/mmcblk0p2
+BOARD_CACHE_FILESYSTEM := auto
+
+# Make sure vold formats the proper sdcard partition
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
---
+++ full_inc.mk
@@ -0,0 +1,141 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS := device/htc/inc/overlay
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us.mk)
+
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/root/init.inc.rc:root/init.inc.rc \
+    device/htc/inc/prebuilt/root/init.inc.usb.rc:root/init.inc.usb.rc \
+    device/htc/inc/prebuilt/root/ueventd.inc.rc:root/ueventd.inc.rc \
+    device/htc/inc/prebuilt/root/fstab.inc:root/fstab.inc \
+    device/htc/inc/prebuilt/bin/hostapd:system/bin/hostapd \
+    device/htc/inc/prebuilt/bin/hostapd_cli:system/bin/hostapd_cli
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.error.receiver.system.apps=com.google.android.feedback \
+	ro.setupwizard.enterprise_mode=1 \
+	ro.com.google.clientidbase=android-verizon \
+	ro.com.google.locationfeatures=1 \
+	ro.cdma.home.operator.alpha=Verizon \
+	ro.cdma.home.operator.numeric=310012 \
+	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+	ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+	ro.config.vc_call_vol_steps=7 \
+	ro.cdma.otaspnumschema=SELC,1,80,99 \
+	ro.telephony.call_ring.multiple=false \
+	ro.telephony.call_ring.delay=3000 \
+	ro.setupwizard.enable_bypass=1 \
+	ro.media.dec.jpeg.memcap=20000000 \
+	ro.media.enc.jpeg.quality=95,85,70
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc
+
+# Optical trackball
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/usr/idc/curcial-oj.idc:system/usr/idc/curcial-oj.idc
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/usr/keylayout/incrediblec-keypad.kl:system/usr/keylayout/incrediblec-keypad.kl \
+    device/htc/inc/prebuilt/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl
+
+# Keymap
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/usr/keychars/incrediblec-keypad.kcm.bin:system/usr/keychars/incrediblec-keypad.kcm.bin
+
+# sysctl parameters
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf
+
+# non-rotational init.d script
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/init.d/02nonrot:system/etc/init.d/02nonrot
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
+
+# APN list
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd
+
+# Prebuilt libcamera for HAL
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/lib/libcamera.so:obj/lib/libcamera.so \
+    device/htc/inc/prebuilt/lib/libcamera.so:system/lib/libcamera.so
+
+#
+# Packages needed for Inc
+#
+# Sensors
+PRODUCT_PACKAGES += \
+    gps.inc \
+    lights.inc \
+    sensors.inc \
+
+# Call the Torch.apk
+PRODUCT_PACKAGES += \
+    Torch
+
+# USB
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.debuggable=1 \
+    persist.sys.usb.config=mass_storage,adb \
+    persist.service.adb.enable=1
+
+# Init post-boot script
+PRODUCT_COPY_FILES += \
+    device/htc/inc/prebuilt/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh
+
+# Recovery
+TARGET_RECOVERY_UI_LIB := librecovery_htc_inc
+
+# Set dirty_ratio for UMS
+PRODUCT_PROPERTY_OVERRIDES += ro.vold.umsdirtyratio=20
+
+# Common qsd8k stuff
+$(call inherit-product, device/htc/inc/qsd8k.mk)
+
+# Vendor file
+$(call inherit-product-if-exists, vendor/htc/inc/inc-vendor.mk)
+
+# Use generic instead of full_base_telephony (smaller)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+# Get a full list of languages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+
+# Set these variables here to overwrite any inherited values.
+PRODUCT_DEVICE := inc
+PRODUCT_NAME := inc
+PRODUCT_BRAND := verizon_wwe
+PRODUCT_MODEL := ADR6300
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_VERSION_MAJOR := 10
+PRODUCT_VERSION_MINOR := 2
+CM_BUILDTYPE := UNOFFICIAL
